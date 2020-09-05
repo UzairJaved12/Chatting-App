cee02e4277c2bed8781d2444c47582ab8444b389
Uzair Javed <malik.hzu@gmail.com>
2020-09-05 11:50:58 +0500

    add button in Registartion

--- app/src/main/java/com/afa/chattingsystem/CreateOtp.java
+++ app/src/main/java/com/afa/chattingsystem/CreateOtp.java
@@ -2,13 +2,67 @@ package com.afa.chattingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateOtp extends AppCompatActivity {
+
+    private Spinner spinner;
+    private EditText editText;
+    FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        auth = FirebaseAuth.getInstance();
+
+
+        spinner = findViewById(R.id.spinnerCountries);
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, CountryData.countryNames));
+
+        editText = findViewById(R.id.editTextPhone);
+
+        findViewById(R.id.buttonContinue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String code = CountryData.countryAreaCodes[spinner.getSelectedItemPosition()];
+
+                String number = editText.getText().toString().trim();
+
+                if (number.isEmpty() || number.length() < 10) {
+                    editText.setError("Valid number is required");
+                    editText.requestFocus();
+                    return;
+                }
+
+                String phonenumber = "+" + code + number;
+
+                Intent intent = new Intent(CreateOtp.this, OtpActivity.class);
+                intent.putExtra("phonenumber", phonenumber);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(this, Mainactivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+            startActivity(intent);
+        }
     }
 }
